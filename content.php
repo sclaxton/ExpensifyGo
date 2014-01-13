75b390ce94382758e6b37513ceb78e9f4078aeba
Spencer Claxton <saclaxton@gmail.com>
2014-01-13 04:18:06 -0600

    restructure markup

--- content.php
+++ content.php
@@ -1,51 +1,49 @@
 <div style="display:none;" id="trans_con">
-    <div id="trans_table_con">
-        <form id="show_form">
-            <input type="hidden" name="command" value="Get">
-            <input type="hidden" name="returnValueList" value="transactionList">
-                <table>
-                    <tr>
-                        <td>
-                            <input required type="date" name="startDate" placeholder="Start Date">
-                        </td>
-                        <td>
-                            <input required type="date" name="endDate" placeholder="End Date">
-                        </td>
-                        <td>
-                            <button type="submit">Show</button>
-                            <button type="reset">Reset</button>
-                            <button type="button" id="show_all">Show All</button>
-                        </td>
-                    </tr>
-                </table>
-        </form>
-        <table id="trans_table"></table>
-    </div>
-    <div id="add_con">
-        <button id="add_trans">Add</button>
-        <form style="display:none;" id="add_form" action="get_proxy.php">
-            <input type="hidden" name="command" value="CreateTransaction">
+    <span>
+        <button id="add_trans">Add</button> | <button id="see_more">See More</button>
+    </span>
+    <form style="display:none;" id="add_form" action="get_proxy.php">
+        <input type="hidden" name="command" value="CreateTransaction">
+        <table>
+            <tr>
+                <td>
+                    <input required type="date" name="created" placeholder="Date">
+                </td>
+                <td>
+                    <input required type="text" name="amount" placeholder="Amount">
+                </td>
+                <td>
+                    <input required type="text" name="merchant" placeholder="Merchant">
+                </td>
+                <td>
+                    <input type="text" name="comment" placeholder="Comments...">
+                </td>
+                <td>
+                    <button type="submit">Add</button>
+                    <button type="reset">Reset</button>
+                    <button type="button" id="cancel_add">Cancel</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form id="show_form">
+        <input type="hidden" name="command" value="Get">
+        <input type="hidden" name="returnValueList" value="transactionList">
             <table>
                 <tr>
                     <td>
-                        <input required type="date" name="created" placeholder="yyyy/mm/dd">
+                        <input required type="date" name="startDate" placeholder="Start Date">
                     </td>
                     <td>
-                        <input required type="text" name="amount" placeholder="Amount">
+                        <input required type="date" name="endDate" placeholder="End Date">
                     </td>
                     <td>
-                        <input required type="text" name="merchant" placeholder="Merchant">
-                    </td>
-                    <td>
-                        <input type="text" name="comment" placeholder="Comments...">
-                    </td>
-                    <td>
-                        <button type="submit">Add</button>
+                        <button type="submit">Show</button>
                         <button type="reset">Reset</button>
-                        <button type="button" id="cancel_add">Cancel</button>
+                        <button type="button" id="show_all">Show All</button>
                     </td>
                 </tr>
             </table>
-        </form>
-    </div>
+    </form>
+    <table id="trans_table"></table>
 </div>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Expensify Transactions</title>
         <link rel="stylesheet" href="css/expensify_app.css" />
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
         <script type="text/javascript" src="js/expensify_app.js"></script>
     </head>
     <body>
